c9307fdca3497d7683446e7952bce2f5591813b9
David Tyler <davet.code@gmail.com>
2012-11-20 17:49:11 +0000

    Added new test to verify that the position of the plane moves by the right amount after velocity changes. Showed up errors with no equals operator on vector

--- SopwithCoreProject/test/test/tyler/sopwith/PlaneStateQuerierTest.scala
+++ SopwithCoreProject/test/test/tyler/sopwith/PlaneStateQuerierTest.scala
@@ -12,10 +12,11 @@ import net.tyler.sopwith.BombReleased
 import net.tyler.sopwith.BuildingDestroyed
 import net.tyler.messaging.MessagePassing
 import net.tyler.messaging.MessagingComponent
+import com.badlogic.gdx.Gdx
 
 class PlaneStateQuerierTest {
 
-  private val VEL_DELTA = 0.01
+  private val FP_DELTA = 0.01
   
   val initialPlaneState = new PlaneState(new ImmutableVector2f(10f, 10f), new ImmutableVector2f(1f, 2f), 0f, 0f, false)
   
@@ -33,20 +34,33 @@ class PlaneStateQuerierTest {
   
   @Test def initialState() {
     
-    new StateTester {
+    new ApplicationTester with StateTester {
       assertEquals(querier.planeState(0), initialPlaneState)
       assertEquals(querier.planeState(100).velocity, initialPlaneState.velocity)
-      assertEquals(querier.planeState(100).angularVelocity, initialPlaneState.angularVelocity, VEL_DELTA)
+      assertEquals(querier.planeState(100).angularVelocity, initialPlaneState.angularVelocity, FP_DELTA)
     }
   }
   
   @Test def singleVelocityChange() {
-    new StateTester {
+    new ApplicationTester with StateTester {
       val newVelocity = new ImmutableVector2f(-1f, -0.5f)
       messagePassing.send(new PlaneVelocityChange(newVelocity, 10))
       
       assertEquals(querier.planeState(9).velocity, initialPlaneState.velocity)
       assertEquals(querier.planeState(11).velocity, newVelocity)
+    }
+  }
+  
+  @Test def positionChangeProgression() {
+    new ApplicationTester with StateTester {
+      val newVelocity = new ImmutableVector2f(-10f, 7f)
+      messagePassing.send(new PlaneVelocityChange(newVelocity, 10))
+      
+      assertEquals(querier.planeState(9).position, initialPlaneState.position)
+      
+      val positionAfter1000ms = querier.planeState(1010).position
+      assertEquals(positionAfter1000ms.x, -10f, FP_DELTA)
+      assertEquals(positionAfter1000ms.y, 7f, FP_DELTA)
     }
   }
 }
